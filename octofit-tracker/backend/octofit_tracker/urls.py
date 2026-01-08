4205826ce4875a9ecdc2fe529e61462a2349e83e
Alfonso Moreno <fonsimore@gmail.com>
2026-01-08 17:48:32 +0000

    Fix: Remove codespace_name logic from urls.py, use standard REST API routing

--- octofit-tracker/backend/octofit_tracker/urls.py
+++ octofit-tracker/backend/octofit_tracker/urls.py
@@ -18,10 +18,6 @@ from django.contrib import admin
 from django.urls import path, include
 from rest_framework import routers
 from .views import UserViewSet, TeamViewSet, ActivityViewSet, WorkoutViewSet, LeaderboardViewSet
-import os
-
-# Get codespace name from environment variable
-codespace_name = os.environ.get('CODESPACE_NAME', '')
 
 router = routers.DefaultRouter()
 router.register(r'users', UserViewSet)
